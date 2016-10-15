901c2579f5f07b92d2841a63dcf2f050141be318
Justin <Justin@Wheatley.local>
2016-10-15 15:28:32 -0400

    edited game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,8 @@
 console.log("JS file is connected to HTML! Woo!")
 
+// Sets the top two cards as queens and 
+// the bottom two as kings
+
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
