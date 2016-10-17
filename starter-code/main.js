65f7d640f8e29393c54ea8c2b4c13c21d1be9db5
Justin <Justin@Wheatley.local>
2016-10-16 23:03:22 -0400

    completed the assignment!

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,54 @@
 console.log("JS file is connected to HTML! Woo!")
 
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+var gameBoard = document.getElementById('game-board');
+
+function createBoard(){
+	for (var i = 0; i < cards.length; i++){
+		var gameCard = document.createElement('div');
+			gameCard.setAttribute('class', 'card');
+			gameBoard.appendChild(gameCard);
+			gameCard.setAttribute('data-card', cards[i]);
+			gameCard.addEventListener('click', isTwoCards);
+		console.log ("Created card # " + (i+1));
+	}
+}
+
+function isTwoCards(){
+  cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') == 'king') {
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>";
+	}
+	else {
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>";
+	}
+	if (cardsInPlay.length == 2) {
+	    isMatch(cardsInPlay);
+	    cardsInPlay = [];
+	}
+}
+
+function isMatch(cardsInPlay) {
+  if (cardsInPlay[0] == cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
+
+createBoard();
+
+
 // Sets the top two cards as queens and 
 // the bottom two as kings
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
 
 // if (cardTwo == cardFour){
 // 	alert("You found a match!");
@@ -15,14 +57,23 @@ var cardFour = "king";
 // 	alert("Whoops, those cards don't match! Try again!");
 // }
 
-for (var i = 0; i < 4; i++) {
-	var gameBoard = document.getElementById('game-board');
-	var gameCard = document.createElement('div');
-		gameCard.setAttribute('class', 'card');
 
-	gameBoard.appendChild(gameCard);
-	console.log ("Created card # " + (i+1));
-}
+// function isMatch(){
+
+// }
+
+// function isTwoCards(){
+// 	cardsInPlay.push(this,getAttribute('data-card'));
+// 	if (cardsInPlay.length == 2){
+// 		isMatch(cardsInPlay);
+// 	}
+// }
+
+// for (var i = 0; i < cards.length; i++){
+// 	gameCard.setAttribute('data-card', cards[i]);
+// 	gameCard.addEventListener('click', isTwoCards);
+// }
+
 
 
 // for (var i = 0; i < 4; i++) {
@@ -38,11 +89,3 @@ for (var i = 0; i < 4; i++) {
 // 	document.getElementById('game-board')[0].appendChild(gameCard);
 // 	console.log ("Created card # " + (i+1));
 // }
-
-
-// First up, let's create a new list item and store it in a variable.
-// var newListItem = document.createElement('div');
-// // Alright! Now let's update the text content of that list item.
-// newListItem.textContent = 'Feed the cat';
-// // And Finally...let's add that list item as a child of the ul.
-// // document.getElementById('game-board')[0].appendChild(newListItem);
